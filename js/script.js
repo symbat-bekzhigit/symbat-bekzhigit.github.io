4b426066db880d9e1002925b8befd485f5a636ae
Symbat Bekzhigit <sb7971@nyu.edu>
2022-08-02 11:49:04 +0600

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,22 @@
+//j-query syntax - so there are some differences from js syntax we learned
+$(function () { // Same as document.addEventListener("DOMContentLoaded"...
+
+  // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
+  $("#navbarToggle").blur(function (event) {
+    var screenWidth = window.innerWidth; //width of the browser window itself, not the entire screen of the monitor
+    if (screenWidth < 768) {
+      // $("#navbarSupportedContent").collapse('hide');
+      $('.collapse').collapse()
+    }
+  });
+
+  // In Firefox and Safari, the click event doesn't retain the focus
+  // on the clicked button. Therefore, the blur event will not fire on
+  // user clicking somewhere else in the page and the blur event handler
+  // which is set up above will not be called.
+  // Refer to issue #28 in the repo.
+  // Solution: force focus on the element that the click event fired on
+  $("#navbarToggle").click(function (event) {
+    $(event.target).focus();
+  });
+});
